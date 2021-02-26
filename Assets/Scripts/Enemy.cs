a3af9fed53bb62c1469998a06df71aa62afb1eae
amundhau <69798009+amundhau@users.noreply.github.com>
2021-02-26 23:14:55 +0100

    Add basic enemies and restructure

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy: MonoBehaviour
+{
+
+    private readonly float gravity = -9.81f;
+
+    public CharacterController controller;
+
+    [Range(-20, 20)]
+    public float MovementSpeed = 5f;
+
+    [Header("Jumping")]
+    [Range(0, 200)]
+    public float jumpForce = 30f;
+    [Range(0, 20)]
+    public float jumpResetTimer = 5f;
+    [Range(1, 5f)]
+    public float fallMultiplier = 2.5f;
+
+    [Header("Physics")]
+    [Range(0.1f, 2)]
+    public float gravityMultiplier = 1f;
+
+    public bool shouldJump;
+    private float verticalSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        // Vertical movement
+        float verticalSpeed = controller.velocity.y;
+        if (controller.isGrounded)
+        {
+            // Reset speed on landing
+            verticalSpeed = -0.1f;
+            jump();
+        }
+        else
+        {
+            //Falls to ground
+            if (verticalSpeed < 0) verticalSpeed += gravity * gravityMultiplier * fallMultiplier;
+            else verticalSpeed += gravity * gravityMultiplier;
+        }
+
+        // Horizontal movement
+        controller.Move(new Vector2(MovementSpeed, verticalSpeed) * Time.fixedDeltaTime);
+    }
+
+    private void jump()
+    {
+        verticalSpeed += jumpForce;
+    }
+}
---
+++ Assets/Scripts/RunningEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningEnemy : MonoBehaviour
+{
+    public CharacterController controller;
+
+    private readonly float gravity = -9.81f;
+    private float verticalSpeed;
+    public bool shouldJump;
+
+    [Range(-20, 20)]
+    public float MovementSpeed = 5f;
+
+    [Range(0, 20)]
+    public float jumpResetTimer = 5f;
+    [Range(1, 5f)]
+    public float fallMultiplier = 2.5f;
+
+    [Header("Physics")]
+    [Range(0.1f, 2)]
+    public float gravityMultiplier = 1f;
+
+
+    private void FixedUpdate()
+    {
+        // Vertical movement
+        float verticalSpeed = controller.velocity.y;
+        if (controller.isGrounded)
+        {
+            // Reset speed on landing
+            verticalSpeed = -0.1f;
+        }
+        else
+        {
+            //Falls to ground
+            if (verticalSpeed < 0) verticalSpeed += gravity * gravityMultiplier * fallMultiplier;
+            else verticalSpeed += gravity * gravityMultiplier;
+        }
+
+        // Horizontal movement
+        controller.Move(new Vector2(MovementSpeed, verticalSpeed) * Time.fixedDeltaTime);
+    }
+}
