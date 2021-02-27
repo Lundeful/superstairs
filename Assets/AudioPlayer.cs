94ded54d6942d2770dc76f32011341ce48889a4a
amundhau <69798009+amundhau@users.noreply.github.com>
2021-02-27 23:16:31 +0100

    Import files to master

---
+++ Assets/AudioPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip jumpClip;
+    public AudioClip pointClip;
+    public AudioClip gameOverClip;
+    public AudioClip hoverClip;
+    public AudioClip clickClip;
+    // Start is called before the first frame update
+
+    public void jumpSound()
+    {
+        source.PlayOneShot(jumpClip);
+    }
+
+    public void pointSound()
+    {
+        source.PlayOneShot(pointClip);
+    }
+
+    public void gameOverSound()
+    {
+        source.PlayOneShot(gameOverClip);
+    }
+
+    public void hoverSound()
+    {
+        source.PlayOneShot(hoverClip);
+    }
+
+    public void clickSound()
+    {
+        source.PlayOneShot(clickClip);
+    }
+}
