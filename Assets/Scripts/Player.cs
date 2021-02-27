9053e145180a710db2a14d37cce2e03eaa737460
lundeful <chrislund90@gmail.com>
2021-02-27 22:01:50 +0100

    added powerups and stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     [Header("Components")]
     public CharacterController controller;
     public Transform gameProgresser;
+    public SpriteRenderer spriteRenderer;
+    public GameObject superSprite;
 
     [Header("PlayerMovement")]
     [Range(1, 60)]
@@ -43,6 +45,17 @@ public class Player : MonoBehaviour
     private bool jumpPressed = false;
     private float framesOnGround = 0f;
     private float velocityRef; // Reference variable used for SmoothDamp
+    private Animator animator;
+    private bool facingRight = true;
+
+    // Superpower
+    private bool isSuper = false;
+    private float powerDuration = 3f;
+    private float powerTimer;
+    private System.Random random;
+    private bool powerAboveGround = false;
+    private int powerRotationSpeed = 15;
+    private Vector3 initialPosition;
 
     private void Start()
     {
@@ -51,7 +64,9 @@ public class Player : MonoBehaviour
             throw new MissingComponentException("Missing vital components in player script!");
         }
 
+        random = new System.Random();
         groundCheck = GameObject.Find("GroundCheck").transform;
+        animator = GetComponent<Animator>();
         MovePlayerToGameProgresser();
     }
 
@@ -66,14 +81,20 @@ public class Player : MonoBehaviour
     {
         playerInput = Input.GetAxis("Horizontal");
         jumpPressed = Input.GetKey(KeyCode.Space);
+        if (Input.GetKey(KeyCode.E)) EnableSuperpower();
+        else if (Input.GetKey(KeyCode.Q)) DisableSuperpower();
     }
 
     private void FixedUpdate()
     {
         HandleJumpTimer();
+        RotatePlayer();
         MovePlayer();
         PreventPlayerOutOfBounds();
+        UpdateAnimatorValues();
+        HandleSuperpower();
     }
+
 
     private void HandleJumpTimer()
     {
@@ -116,9 +137,76 @@ public class Player : MonoBehaviour
         controller.Move(new Vector2(GetHorizontalMovement(), GetVerticalMovement()) * Time.fixedDeltaTime);
     }
 
+    private void RotatePlayer()
+    {
+        if (!isSuper)
+        {
+            if (controller.velocity.x > 0 && !facingRight) Flip();
+            else if (controller.velocity.x < 0 && facingRight) Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+
     private void PreventPlayerOutOfBounds()
     {
         var distanceFromCenter = (gameProgresser.position - controller.transform.position).magnitude;
         if (distanceFromCenter > playerResetDistance) MovePlayerToGameProgresser();
+    }
+    private void UpdateAnimatorValues()
+    {
+        animator.SetFloat("velX", controller.velocity.x);
+        animator.SetBool("inAir", !controller.isGrounded);
+    }
+
+    private void EnableSuperpower()
+    {
+        isSuper = true;
+        powerTimer = powerDuration;
+        powerRotationSpeed = random.Next(5, 20);
+        powerAboveGround = powerRotationSpeed >= 10;
+        spriteRenderer.enabled = false;
+        superSprite.SetActive(true);
+    }
+
+    private void DisableSuperpower()
+    {
+        isSuper = false;
+        spriteRenderer.enabled = true;
+        superSprite.SetActive(false);
+    }
+
+    private void HandleSuperpower()
+    {
+        powerTimer -= 1 * Time.fixedDeltaTime;
+        if (isSuper && powerTimer > 0f)
+        {
+            superSprite.transform.Rotate(0, 0, powerRotationSpeed);
+            if (powerAboveGround) superSprite.transform.position = this.transform.position + Vector3.up * 7 + new Vector3(0f, Mathf.Sin(Time.time * 2), 1);
+            else superSprite.transform.position = this.transform.position + Vector3.down * 5;
+        }
+        else if (isSuper && powerTimer <=  0f)
+        {
+            DisableSuperpower();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Drops")
+        {
+            EnableSuperpower();
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 11) Debug.Log("Enemy");
     }
 }
