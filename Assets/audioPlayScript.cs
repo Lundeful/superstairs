1433704d42838388af33772bc63f647d36cf54b0
amundhau <69798009+amundhau@users.noreply.github.com>
2021-02-27 17:37:24 +0100

    Add menu audio

---
+++ Assets/audioPlayScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioPlayScript : MonoBehaviour
+{
+    public AudioSource buttonSounds;
+    public AudioClip hoverClip;
+    public AudioClip clickClip;
+    // Start is called before the first frame update
+    
+    public void HoverSound()
+    {
+        buttonSounds.PlayOneShot(hoverClip);
+    }
+
+    public void ClickSound()
+    {
+        buttonSounds.PlayOneShot(clickClip);
+    }
+}
